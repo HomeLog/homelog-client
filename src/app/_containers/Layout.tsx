98fc064fb24a7085bc8a643042147c0d989a6fbc
jimyungkoh <kojimyung1206@naver.com>
2024-05-26 17:39:46 +0900

    style: 스크롤바 숨김 처리

--- src/app/_containers/Layout.tsx
+++ src/app/_containers/Layout.tsx
@@ -3,10 +3,9 @@ import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
 
-
 function Background({ children }: { children: ReactNode }) {
   return (
-    <div className='max-w-md min-h-screen mx-auto bg-white'>
+    <div className='flex flex-col h-full max-w-md mx-auto bg-white'>
       <Header />
       <Main>{children}</Main>
       <Footer />
--- src/app/_containers/Main.tsx
+++ src/app/_containers/Main.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 function Main({ children }: { children: ReactNode }) {
   return (
-    <main className='flex flex-col items-center justify-between h-screen px-10'>
+    <main className='flex flex-col items-center justify-between flex-grow bg-inherit'>
       {children}
     </main>
   );
