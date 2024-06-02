1932b5d4bdb543d44655e1663e5344468be2faeb
jimyungkoh <kojimyung1206@naver.com>
2024-06-02 20:13:20 +0900

    feat: util 함수 정의
    - file을 imageFile 타입으로 변경해주는 유틸 함수와 filePath로부터 생성일자를 알아내는 유틸 함수

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,6 +1,26 @@
+import ImageFile from '@/types/image.file';
 import clsx, { ClassValue } from 'clsx';
+import fs from 'fs/promises';
 import { twMerge } from 'tailwind-merge';
 
 export function mergeClassNames(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function getCreatedAt(file: string): Promise<Date> {
+  const { birthtime } = await fs.stat(file);
+  return birthtime;
+}
+
+export function convertFileToImageFile(file: File): ImageFile {
+  const result = {
+    ...file,
+    previewUrl: URL.createObjectURL(file),
+    date: new Date(file.lastModified)
+      .toLocaleDateString('ko-KR')
+      .replace(/\./g, '')
+      .replace(/ /g, '.'),
+  };
+
+  return result;
 }
