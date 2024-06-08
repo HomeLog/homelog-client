60cf18280d53c52fe1fcf1580de452342c891e92
jimyungkoh <kojimyung1206@naver.com>
2024-06-08 16:03:15 +0900

    fix: 클라이언트 측 쿠키 저장 문제 해결
    - Kakao 로그인 콜백 URL을 API 호출하도록 수정

--- src/app/users/kakao/callback/page.tsx
+++ src/app/users/kakao/callback/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import api from '@/app/api';
 import useAuth from '@/contexts/auth.context';
-import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -16,7 +15,7 @@ const KakaoCallbackPage = ({
 
   useEffect(() => {
     const signInAndRedirect = async () => {
-      await `${process.env.NEXT_PUBLIC_SERVER_URL}/users/kakao/callback?code=${code}`;
+      await api.auth.signInKakao(code);
       signIn();
       router.push('/');
     };
