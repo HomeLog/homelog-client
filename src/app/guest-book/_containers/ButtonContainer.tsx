61c27b4dddbef0354bd0bd08f64aef88cf729d36
sangddong <hehe74029@gmail.com>
2024-06-11 16:40:41 +0900

    fix: ButtonContainer 관련 오류 수정

--- src/app/guest-book/_containers/ButtonContainer.tsx
+++ src/app/guest-book/_containers/ButtonContainer.tsx
@@ -5,13 +5,17 @@ import { mergeClassNames } from '@/libs/utils';
 function ButtonContainer({
   buttonText,
   className,
+  onClick,
 }: {
   buttonText: string;
   className?: string;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }) {
   return (
     <Flex className={mergeClassNames(className)}>
-      <Button intent='primary'>{buttonText}</Button>
+      <Button intent='primary' onClick={onClick}>
+        {buttonText}
+      </Button>
     </Flex>
   );
 }
