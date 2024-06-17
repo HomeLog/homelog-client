7bc02124483373bcc65572b999c7bf4e2c24992e
sangddong <hehe74029@gmail.com>
2024-06-17 22:25:09 +0900

    chore: 홈화면 css 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,8 @@ import Flex from '@/components/Flex';
 import { useEffect } from 'react';
 import HomeButton from './_containers/HomeButton';
 import useQueryGetProfile from '@/hooks/profile/useQuery.getProfile';
+import Polaroid from './_components/Polaroid';
+import Grid from '@/components/Grid';
 
 function Home() {
   const router = useRouter();
@@ -15,6 +17,7 @@ function Home() {
   const guestbookName = profile?.guestBookName;
   const homeImage = profile?.homeImageUrl ?? '/images/background.png';
   const profileImage = profile?.profileImageUrl ?? '/images/blank-profile.png';
+  const totalGuestbook = 0;
 
   useEffect(() => {
     if (loading === false && isLoggedIn === false) {
@@ -38,7 +41,7 @@ function Home() {
   return (
     <div className='bg-[#F5F5F5] w-full h-full'>
       <Flex className='w-full h-1/3 relative drop-shadow-lg'>
-        <div className='w-full h-full relative'>
+        <div id='home image' className='w-full h-full relative'>
           <Image
             src={homeImage}
             alt='Home Image'
@@ -47,26 +50,39 @@ function Home() {
             className='w-full'
           />
         </div>
-        <div className='absolute bg-inherit aspect-square w-[14%]'>
-          <Image
-            src={profileImage}
-            alt='Profile Image'
-            fill
-            objectFit='cover'
-            className='rounded-full'
-          />
-        </div>
-        <div className='absolute top-16 font-bold	text-2xl text-white	'>
-          {guestbookName}
-        </div>
-        <Flex className='absolute bottom-5 flex flex-row justify-evenly w-full items-end'>
-          <HomeButton onClick={redirectToProfileEditPage}>
-            프로필 편집
-          </HomeButton>
-          <HomeButton onClick={redirectToGenerateLinkPage}>
-            링크 생성
-          </HomeButton>
-          <HomeButton onClick={logOut}>로그아웃</HomeButton>
+        <Flex className='absolute w-full h-full'>
+          <div
+            id='guestbook name'
+            className='relative font-bold	text-2xl text-white	mt-16'
+          >
+            {guestbookName}
+          </div>
+          <div
+            id='profile image'
+            className='relative aspect-square w-[14%] rounded-full overflow-hidden mt-5'
+          >
+            <Image
+              src={profileImage}
+              alt='Profile Image'
+              layout='fill'
+              objectFit='cover'
+            />
+          </div>
+          <div
+            id='total'
+            className='relative text-sm text-white font-thin mt-1 mb-12'
+          >
+            Total: {totalGuestbook}
+          </div>
+          <Flex className='relative flex flex-row justify-evenly w-full items-end'>
+            <HomeButton onClick={redirectToProfileEditPage}>
+              프로필 편집
+            </HomeButton>
+            <HomeButton onClick={redirectToGenerateLinkPage}>
+              링크 생성
+            </HomeButton>
+            <HomeButton onClick={logOut}>로그아웃</HomeButton>
+          </Flex>
         </Flex>
       </Flex>
     </div>
