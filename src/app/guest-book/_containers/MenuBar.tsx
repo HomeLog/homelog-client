9c85fb6e98c81944adf40b21e7e14a8d89e7ef23
jimyungkoh <kojimyung1206@naver.com>
2024-06-25 17:21:21 +0900

    feat: menuBar 라우트 기능 추가

--- src/app/guest-book/_containers/MenuBar.tsx
+++ src/app/guest-book/_containers/MenuBar.tsx
@@ -3,11 +3,23 @@
 import Button from '@/components/Button';
 import { mergeClassNames } from '@/libs/utils';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import deleteIconSrc from '/public/icons/delete.svg?url';
 import editIconSrc from '/public/icons/edit.svg?url';
 import homeIconSrc from '/public/icons/home.svg?url';
 
+// TODO: delete 기능 구현
 function MenuBar({ className }: { className: string }) {
+  const router = useRouter();
+
+  const handleClickHome = () => {
+    router.push('/');
+  };
+
+  const handleClickEdit = () => {
+    router.push('/guest-book/create');
+  };
+
   return (
     <div
       className={mergeClassNames(
@@ -16,12 +28,12 @@ function MenuBar({ className }: { className: string }) {
       )}
     >
       <div className='relative flex aspect-square'>
-        <Button intent='none'>
+        <Button intent='none' onClick={handleClickHome}>
           <Image src={homeIconSrc} alt='home' fill className='object-contain' />
         </Button>
       </div>
       <div className='relative flex aspect-square'>
-        <Button intent='none'>
+        <Button intent='none' onClick={handleClickEdit}>
           <Image src={editIconSrc} alt='edit' fill className='object-contain' />
         </Button>
       </div>
