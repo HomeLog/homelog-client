d426e4f1a81f5e314301febfdea57b7d770eb35f
sangddong <hehe74029@gmail.com>
2024-06-26 18:04:09 +0900

    chore: css 수정

--- src/app/_components/GuestbookList.tsx
+++ src/app/_components/GuestbookList.tsx
@@ -7,7 +7,7 @@ import Grid from '@/components/Grid';
 function GuestbookList({ guestbooks }: { guestbooks: DGuestBook[] }) {
   return (
     <>
-      <div className='bg-[#F5F5F5] px-3 mt-12 '>
+      <div className='px-3 mt-12'>
         <Grid className='mx-auto grid-cols-3 w-[95%] h-min items-center'>
           {guestbooks?.map((guestbook: DGuestBook, index: number) => (
             <Polaroid key={index} {...guestbook} />
