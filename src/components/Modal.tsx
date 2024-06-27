c47043d4e18b3b213462b1a4db9a9f32313727d1
sangddong <hehe74029@gmail.com>
2024-06-27 15:06:24 +0900

    refactor: Modal 컴포넌트 수정 및

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,42 +1,29 @@
-import Button from './Button';
-
-const buttonStyles = {
-  intent: 'secondary' as 'secondary',
-  size: 'md' as 'md',
-  rounded: 'sm' as 'sm',
-};
-
 interface ModalProps {
-  clickModal: () => void;
-  setBasicImage: () => void;
-  selectImage: () => void;
-  isAvatarImage: boolean;
+  setModal: () => void;
+  title?: string;
+  children?: React.ReactNode;
 }
 
-const Modal: React.FC<ModalProps> = ({
-  clickModal,
-  setBasicImage,
-  selectImage,
-  isAvatarImage,
-}) => {
+const Modal = ({ setModal, title, children }: ModalProps) => {
+  const preventOffModal = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return (
     <div
+      onClick={setModal}
       className='absolute w-full h-full bg-gray-500/50 p-10'
-      onClick={clickModal}
     >
       <div
-        onClick={clickModal}
+        onClick={preventOffModal}
         className='fixed bg-white rounded-md transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 px-2 py-5'
       >
-        <div className='font-bold text-gray-400 w-full px-7 py-4'>
-          {isAvatarImage ? '프로필' : '홈'} 사진 설정
-        </div>
-        <Button onClick={setBasicImage} {...buttonStyles}>
-          기본 이미지 적용
-        </Button>
-        <Button onClick={selectImage} {...buttonStyles}>
-          앨범에서 사진 선택
-        </Button>
+        {title && (
+          <div className='font-bold text-gray-400 w-full px-7 py-4'>
+            {title}
+          </div>
+        )}
+        {children}
       </div>
     </div>
   );
