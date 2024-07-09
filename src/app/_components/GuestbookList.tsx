51a494aeae9b5910f169629f5c6954d39d6e457c
jimyungkoh <kojimyung1206@naver.com>
2024-07-09 17:21:40 +0900

    refactor: 방명록 목록 컴포넌트 및 쿼리 로직 개선

--- src/app/_components/GuestbookList.tsx
+++ src/app/_components/GuestbookList.tsx
@@ -1,11 +1,11 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
-import Polaroid from './Polaroid';
-import { DGuestBook } from '@/types/guestbook.type';
-import Grid from '@/components/Grid';
-import Flex from '@/components/Flex';
 import { getAllGuestbooks } from '@/api/guestbook/guestbook.api';
+import Flex from '@/components/Flex';
+import Grid from '@/components/Grid';
 import useQueryGetDataPerPage from '@/hooks/guestbook/useQuery.getGuestbooks';
+import { DGuestBook } from '@/types/guestbook.type';
+import { useEffect, useRef } from 'react';
+import Polaroid from './Polaroid';
 
 const LIMIT_PER_PAGE = 15;
 
@@ -14,13 +14,23 @@ function GuestbookList() {
     return await getAllGuestbooks(pageParam, LIMIT_PER_PAGE);
   };
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useQueryGetDataPerPage({
-      fetchData: fetchComments,
-      limit: LIMIT_PER_PAGE,
-    });
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    refetch: refetchGuestbooks,
+  } = useQueryGetDataPerPage({
+    fetchData: fetchComments,
+    limit: LIMIT_PER_PAGE,
+    queryKey: ['guestbooks'],
+  });
 
   const loadMoreRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    refetchGuestbooks();
+  }, [refetchGuestbooks]);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -45,12 +55,12 @@ function GuestbookList() {
     <div className='px-3 mt-12'>
       <Grid className='mx-auto grid-cols-3 w-[95%] h-min items-center'>
         {data?.pages.flatMap((page) =>
-          page.map((guestbook: DGuestBook, index: number) => (
-            <Polaroid key={index} {...guestbook} />
+          page.map((guestbook: DGuestBook) => (
+            <Polaroid key={guestbook.id} {...guestbook} />
           )),
         )}
       </Grid>
-      <Flex className='my-5 text-gray-500 font-thin'>
+      <Flex className='my-5 font-thin text-gray-500'>
         <div ref={loadMoreRef}>{isFetchingNextPage ? 'Loading...' : ''}</div>
       </Flex>
     </div>
--- src/hooks/guestbook/useQuery.getGuestbooks.tsx
+++ src/hooks/guestbook/useQuery.getGuestbooks.tsx
@@ -3,14 +3,16 @@ import { useInfiniteQuery } from '@tanstack/react-query';
 interface PaginationProps {
   fetchData: ({ pageParam }: { pageParam?: number }) => Promise<any>;
   limit: number;
+  queryKey: string[];
 }
 
 export default function useQueryGetDataPerPage({
   fetchData,
   limit,
+  queryKey
 }: PaginationProps) {
   return useInfiniteQuery({
-    queryKey: ['data'],
+    queryKey: queryKey,
     queryFn: fetchData,
     getNextPageParam: (currentPage, allPages) => {
       if (currentPage.length < limit) return undefined;
