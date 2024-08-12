97c7c2cb28444e00a1ff8f6862b7718e8d582706
jimyungkoh <kojimyung1206@naver.com>
2024-08-13 02:22:54 +0900

    fix: safari 환경에서 이미지 로드 안되는 현상 수정
    - 여러번 유틸 메서드를 호출해서 해결하는 방식인데, 근본적인 해결책이라고는 할 수 없음
    - 추후 더 나은 해결법이 나온다면 업데이트 예정

--- src/app/guest-book/_utils/image.util.ts
+++ src/app/guest-book/_utils/image.util.ts
@@ -2,14 +2,29 @@ import TImageFile from '@/types/image.file';
 import * as htmlToImage from 'html-to-image';
 
 export const processImage = async () => {
-  const imageUploadContainer = document.querySelector(
-    '#image-upload-container',
-  ) as HTMLElement;
-  const image = await htmlToImage.toJpeg(imageUploadContainer, {
-    quality: 1,
-  });
-  const base64Image = await fetch(image);
-  return await base64Image.blob();
+  const jpegDataUrl = await buildJpeg('image-upload-container');
+
+  const base64Image = await fetch(jpegDataUrl);
+
+  return base64Image.blob();
+};
+
+const buildJpeg = async (elementId: string) => {
+  const element = document.getElementById(elementId) as HTMLElement;
+
+  let dataUrl = '';
+  const minDataLength = 2000000;
+  let i = 0;
+  const maxAttempts = 10;
+
+  while (dataUrl.length < minDataLength && i < maxAttempts) {
+    dataUrl = await htmlToImage.toJpeg(element, {
+      quality: 1,
+    });
+    i += 1;
+  }
+
+  return dataUrl;
 };
 
 export function convertFileToImageFile(file: File): TImageFile {
